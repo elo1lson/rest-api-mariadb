7b8b31a7adb267978545367c230a293d6e03bbc5
elo1lson <eloilsonfontenele2@gmail.com>
2022-10-05 20:13:35 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express")
+const morgan = require('morgan')
+const app = express()
+
+const routaProdutos = require("./routes/produtos")
+
+app.use(morgan('dev'))
+app.use('/produtos', routaProdutos)
+
+app.use((req, res, next) => {
+  let erro = new Error('Not found')
+  erro.status = 404
+  next(erro)
+})
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500)
+  return res.send({
+    erro:{
+      message: error.message
+    }
+  })
+})
+
+module.exports = app 
